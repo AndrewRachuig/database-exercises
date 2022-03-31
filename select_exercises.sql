8994941272ff4eed0c4cd04bd51d0b1bcc46cfb9
Andrew Rachuig <andrew.rachuig@gmail.com>
2022-03-31 09:29:19 -0500

    updated these exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,32 +1,98 @@
 use albums_db;
 
-select * from albums;
+
+SELECT 
+    *
+FROM
+    albums;
+
 
 -- a. How many rows are in the albums table?
+SELECT 
+    COUNT(*)
+FROM
+    albums;
 -- 	  31 rows
+
+
 -- b. How many unique artist names are in the albums table?
+SELECT 
+    COUNT(DISTINCT artist)
+FROM
+    albums;
 -- 	  23 unique artist names
+
+
 -- c. What is the primary key for the albums table?
+SHOW KEYS FROM albums WHERE Key_name = 'PRIMARY';
 -- 	  The id column is the primary key
+
+
 -- d. What is the oldest release date for any album in the albums table? What is the most recent release date?
+SELECT 
+    name, release_date
+FROM
+    albums
+ORDER BY release_date ASC
+LIMIT 1;
+
+SELECT 
+    name, release_date
+FROM
+    albums
+ORDER BY release_date DESC
+LIMIT 1;
 --    Oldest release date is  'Sgt. Pepper''s Lonely Hearts Club Band' and the most recent is '21'
+
+
 
 -- Write queries to find the following information:
 
 -- a. The name of all albums by Pink Floyd
-SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT 
+    name
+FROM
+    albums
+WHERE
+    artist = 'Pink Floyd';
+
 
 -- b. The year Sgt. Pepper's Lonely Hearts Club Band was released
-SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT 
+    release_date
+FROM
+    albums
+WHERE
+    name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
+
 
 -- c. The genre for the album Nevermind
-SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT 
+    genre
+FROM
+    albums
+WHERE
+    name = 'Nevermind';
+
 
 -- d. Which albums were released in the 1990s
-SELECT name, release_date FROM albums WHERE release_date > 1989 and release_date < 2000;
+SELECT 
+    name, release_date
+FROM
+    albums
+WHERE
+    release_date > 1989
+        AND release_date < 2000;
+
 
 -- e. Which albums had less than 20 million certified sales
-SELECT name, sales FROM albums WHERE sales < 20;
+SELECT 
+    name, sales
+FROM
+    albums
+WHERE
+    sales < 20;
+
 
 -- f. All the albums with a genre of "Rock". 
 SELECT 
@@ -35,10 +101,24 @@ FROM
     albums
 WHERE
     genre LIKE '%Rock%'
-        AND genre NOT LIKE 'Hard rock'
+        AND genre NOT LIKE '%Hard rock%'
         AND genre NOT LIKE '%Alternative rock%'
         AND genre NOT LIKE '%Progressive rock%'
         AND genre NOT LIKE '%Soft rock%'
-        AND genre NOT LIKE '%Folk rock%';
+        AND genre NOT LIKE '%Folk rock%'
+        or genre LIKE "Rock, Soft Rock%";
 -- Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
--- Because "Hard rock" and "Progressive rock" are separate genre
+
+/* I'm assuming you mean why does the query 
+
+SELECT 
+    name, genre
+FROM
+    albums
+WHERE
+    genre = 'Rock'; 
+
+not return those rows with albums of genre "Hard rock" or "Progressive rock", 
+that would be because they don't match the searched for term "Rock" exactly, but my query doesn't contain them as I exclude them explicitly. */
+
+Select name, genre FROM albums WHERE genre LIKE "%Rock%";
